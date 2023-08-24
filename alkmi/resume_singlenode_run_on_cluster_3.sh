fabc5be7ad160bbd22a45007fec474f985d69938
theodor <amariucaitheodor@protonmail.com>
2023-08-24 13:52:38 +0200

    Minor fixes x2 regarding the resume configurations
    Signed-off-by: theodor <amariucaitheodor@protonmail.com>

--- alkmi/resume_singlenode_run_on_cluster_3.sh
+++ alkmi/resume_singlenode_run_on_cluster_3.sh
@@ -4,7 +4,8 @@ RESUME_ID=${1-}
 WANDB_RUN_GROUP=${2-"text1-vision1"}
 NUM_GPUS=${3-1}
 VRAM_PER_GPU=${4-40g}
-echo "Selected configuration: configs/flava/wit_resume_model_3.yaml, GPUs: $NUM_GPUS, $VRAM_PER_GPU VRAM/GPU, resuming run $RESUME_ID (ID) in group '$WANDB_RUN_GROUP'"
+CONFIG="wit_resume_model_3.yaml"
+echo "Selected configuration: configs/flava/$CONFIG, GPUs: $NUM_GPUS, $VRAM_PER_GPU VRAM/GPU, resuming run $RESUME_ID (ID) in group '$WANDB_RUN_GROUP'"
 
 # 5 days is the upper limit on Euler (before PartitionTimeLimit kicks in)
 
@@ -22,7 +23,7 @@ sbatch --job-name="bash" \
   --mem-per-cpu=10000 \
   --gres=gpumem:"$VRAM_PER_GPU" \
   --output "resume_singlenode_$(date "+%F-%T").log" \
-  --wrap="WANDB_RUN_GROUP=$WANDB_RUN_GROUP WANDB__SERVICE_WAIT=300 WANDB_RESUME=must WANDB_RUN_ID=$RESUME_ID NUMEXPR_MAX_THREADS=64 python -m train config=configs/flava/wit_resume_model.yaml"
+  --wrap="WANDB_RUN_GROUP=$WANDB_RUN_GROUP WANDB__SERVICE_WAIT=300 WANDB_RESUME=must WANDB_RUN_ID=$RESUME_ID NUMEXPR_MAX_THREADS=64 python -m train config=configs/flava/$CONFIG"
 
 # eu-a65-03 might have an issue
 # --nodelist=eu-a65-02,eu-a65-04,eu-a65-05,eu-a65-07 \
