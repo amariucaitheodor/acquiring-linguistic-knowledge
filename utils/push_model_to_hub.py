b32edf020f68d922d90a96489afa5578a9b20d5c
theodor <amariucaitheodor@protonmail.com>
2023-08-27 21:57:59 +0200

    Minor changes
    Signed-off-by: theodor <amariucaitheodor@protonmail.com>

--- utils/push_model_to_hub.py
+++ utils/push_model_to_hub.py
@@ -4,16 +4,16 @@ from alkmi.models.lightning_flava import FlavaPreTrainingLightningModule
 from alkmi.models.flava import FlavaProcessor
 from alkmi.models.flava import FlavaForPreTraining
 
-NAME = "flava_text_only_2"
-CKPT_LOCATION = "text10-vision0/bs4096_seed5501650_bf16-mixed/last.ckpt"
+NAME = "thesis_halfsize_text1_vision0"
+CKPT_LOCATION = "text1-vision0/half_bs4032_seed5501650_bf16-mixed/flava-epoch=00-step=5654.ckpt"
 
 tokenizer: BertTokenizerFast = FlavaProcessor.from_pretrained("facebook/flava-full").tokenizer
 tokenizer.save_pretrained(f"theodor1289/{NAME}", push_to_hub=True)
 
 model: FlavaForPreTraining = FlavaPreTrainingLightningModule.load_from_checkpoint(
     f"/cluster/work/cotterell/tamariucai/HuggingfaceCheckpoints/flava-wit/{CKPT_LOCATION}",
-    # arguments below are irrelevant
-    pretrained=False, half_size=False,
+    # arguments below are irrelevant but needed to load
+    pretrained=False, half_size=True,
     learning_rate=0.001, adam_eps=1e-8, adam_weight_decay=0.01,
     adam_betas=(0.9, 0.999), warmup_steps=10000, max_steps=450000,
 ).model
